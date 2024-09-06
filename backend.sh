216f1ad21a1bed7fef05345d3a6d18b0f3c7d817
harshavardhan <kadiamharsha27@gmail.com>
2024-09-06 21:25:57 +0530

    Automating using shell-script - backend

--- backend.sh
+++ backend.sh
@@ -68,4 +68,19 @@ VALIDATE $? "Extracting backend application code"
 npm install &>>$LOG_FILE
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service # ABSOLUTE PATH
 
+# Load the data before running the backend
 
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.harshadaws81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+VALIDATE $? "Schema Loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon-reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted backend"
